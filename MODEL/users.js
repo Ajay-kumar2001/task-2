0e6e4db0cd929becd611b3d4312111a0281f4fea
Ajaykumar <102914283+Ajay-kumar2001@users.noreply.github.com>
2023-05-15 10:21:23 +0530

    added

---
+++ MODEL/users.js
@@ -0,0 +1,8 @@
+let mongoose=require("mongoose")
+const sehema=new mongoose.Schema({
+    name:{type:String,require:true},
+    password:{type:String ,require:true},
+    conformpassword:{type:String ,require:true}
+})
+module.exports=mongoose.model("users",sehema)
+
